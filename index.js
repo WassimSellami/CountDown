86f7d0eeb0e9445ee4ee36c3341e881aa73cf91f
hamza faidi <hamza_faidi@outlook.fr>
2022-09-17 17:44:19 +0100

    commit 3

---
+++ index.js
@@ -0,0 +1,14 @@
+// const timetxt = doument.getElementById("time") ; 
+const startButton = doument.getElementById("start-btn") ; 
+const title = doument.getElementById("welcome") ; 
+// startButton.addEventListener("keyup", ()=>{
+
+// })
+startButton.onclick = ()=>{
+    startButtonClicked() ; 
+}
+
+const startButtonClicked=  ()=>{ 
+     timetxt = 50 
+     console.log("hello") 
+}
