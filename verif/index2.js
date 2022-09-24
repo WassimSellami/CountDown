66e5bb480c65f62cacff7c51fd613f7a3067b617
hamza faidi <hamza_faidi@outlook.fr>
2022-09-24 14:19:40 +0100

    adding verification page

--- verif/index2.js
+++ verif/index2.js
@@ -1,25 +1,26 @@
 var timetxt = document.getElementById("time") ; 
-const sendButton = document.getElementById("start-btn") ; 
+const sendButton = document.getElementById("send-btn") ; 
 const title = document.getElementById("welcome") ; 
 const codeInput = document.getElementById("code-input") ;
 const input = document.getElementById("input") ;
 const sendAgainButton = document.getElementById("send-again-btn")
 const submitButton = document.getElementById("submit-btn")
 const form = document.getElementById("form")
-
-var initialTime 
+const code = 1234 
+const timeout = 3 
+var restartCounter  
 var timeLeft
 function init(){
      sendAgainButton.style.display= "none"
      form.style.display= "none"
-
+     sendButton.style.display = "block"
      //renderTime(timeLeft) 
 }
 
 function startCounting(){
     if(timeLeft >=0 ){
         
-        setTimeout(startCounting, 1000) ; 
+        restartCounter = setTimeout(startCounting, 1000) ; 
         renderTime(timeLeft)
         timeLeft -- 
     }
@@ -36,28 +37,59 @@ function startCounting(){
 
 // })
 sendButton.onclick = ()=>{
-    startButtonClicked() ; 
+    sendButtonClicked() ; 
+
 }
 sendAgainButton.onclick= ()=>{
-    init() 
-    startButtonClicked()
+   
+    sendButtonClicked()
     
 }
+submitButton.onclick = () => {
+    submitButtonClicked() 
+}
 
-const startButtonClicked=  ()=>{  
-    title.style.display = "none"; 
+function AcceptedVerification() { 
+    clearTimeout(restartCounter)
+    title.innerText = "Accepted" 
+    sendAgainButton.style.display = "none";
+    form.style.display = "none";
+
+}
+
+function failedVerification(){
+    codeInput.value = ""
+    window.alert("Wrong Code") 
+
+}
+
+const submitButtonClicked = () => {
+    var codeFromForm = codeInput.value  
+    if(code == codeFromForm ) {
+        AcceptedVerification()
+    }
+    else {
+        failedVerification()
+    }
+}
+
+const sendButtonClicked=  ()=>{  
+    window.alert(code) 
+    clearTimeout(restartCounter)
     sendButton.style.display = "none";
-    input.style.display = "none";
-    initialTime = codeInput.value
-    timeLeft = initialTime
+    sendAgainButton.style.display = "block";
+    form.style.display = "block";
+    
+    timeLeft = timeout 
 
     startCounting() 
 }
 function countingDone(){
+    window.alert("Timeout is up")
+    init()
     title.style.display = "block";
     input.style.display = "flex";
-
-    sendAgainButton.style.display= "block" 
+    sendAgainClicked.style.display= "block" 
 }
 
 init()
